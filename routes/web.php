4e2c465725003aef6f9a17469ffd95c824cf69a7
ClaudioSiburian <claudiohuge26@gmail.com>
2026-05-11 12:01:21 +0700

    pertemuan 11

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,8 @@ Route::get('blog', function () {
 Route::get('pertemuan5', function () {
 	return view('pertemuan5');
 });
+
+Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+Route::get('/formulir', [PegawaiController::class, 'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+Route::get('/pegawai', [PegawaiDBController::class, 'index']);
